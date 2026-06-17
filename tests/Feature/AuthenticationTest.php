743de940a4fbb2d444997f71abe86eb6f57f5984
Miguel Angel Ruiz <99372164+aruiz2807@users.noreply.github.com>
2026-06-17 17:18:56 -0600

    fix(proxy): honor https and subdirectory behind alias

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -8,6 +8,21 @@ test('login screen can be rendered', function () {
     $response->assertStatus(200);
 });
 
+test('login screen generates https urls behind trusted proxies', function () {
+    $response = $this
+        ->withServerVariables([
+            'HTTP_HOST' => 'caborcaautomotriz.qualisys.mx',
+            'HTTP_X_FORWARDED_HOST' => 'caborcaautomotriz.qualisys.mx',
+            'HTTP_X_FORWARDED_PROTO' => 'https',
+            'HTTP_X_FORWARDED_PREFIX' => '/citas',
+        ])
+        ->get('/login');
+
+    $response->assertOk();
+    $response->assertSee('https://caborcaautomotriz.qualisys.mx/citas', false);
+    $response->assertDontSee('http://caborcaautomotriz.qualisys.mx/citas', false);
+});
+
 test('users can authenticate using the login screen', function () {
     $user = User::factory()->create();
 
@@ -17,7 +32,7 @@ test('users can authenticate using the login screen', function () {
     ]);
 
     $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
+    $response->assertRedirect(route('home', absolute: false));
 });
 
 test('users cannot authenticate with invalid password', function () {
